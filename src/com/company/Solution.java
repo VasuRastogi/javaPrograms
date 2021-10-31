0cc8ac79420cfae7dc0955225d64a9fe10c10d6c
VasuRastogi <vasurastogi77@gmail.com>
2021-10-31 17:47:42 +0530

    name of kunal is changed to GithubDSA

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,33 +1,28 @@
 package com.company;
-public class Solution {
+import java.io.*;
+import java.io.Serializable;
 
-    public static void main(String []args){
-        int []arr=new int[]{1,5,7,8,9};
-        int targt=4;
-        int store=binarySearch(arr, targt);
-        System.out.println(store);
+class Student implements Serializable{
+    int id;
+    String name;
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    public static int binarySearch(int[] nums, int target) {
-        if (nums == null || nums.length == 0)
-            return -1;
-
-        int left = 0, right = nums.length;
-        while (left < right) {
-            // Prevent (left + right) overflow
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+}
+public class Solution {
+    public static void main(String args[]) {
+        try {
+            Student s1 = new Student(211, "Vasu");
+            FileOutputStream fout = new FileOutputStream("newtxt");
+            ObjectOutputStream out = new ObjectOutputStream(fout);
+            out.writeObject(s1);
+            out.flush();
+            out.close();
+            System.out.println("Done!!");
+        } catch (Exception e) {
+            System.out.println(e);
         }
-
-        // Post-processing:
-        // End Condition: left == right
-        if (left != nums.length && nums[left] == target) return left;
-        return -1;
     }
 }
--- src/com/company/bubbleSort.java
+++ src/com/company/bubbleSort.java
@@ -1,7 +1,22 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class bubbleSort {
     public static void main(String[]args){
-        int[] arr = new int[]{};
+        int[] arr = new int[]{1,2,5,4,3,6};
+        bubbleSrt(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+    static void bubbleSrt(int[] arr){
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr.length-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
     }
 }
