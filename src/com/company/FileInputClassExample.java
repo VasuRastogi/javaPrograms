2823d5349c84cee11e90e5bf1e8e92144bc158e9
VasuRastogi <vasurastogi77@gmail.com>
2021-12-03 16:06:15 +0530

    warnings removed.

--- src/com/company/FileInputClassExample.java
+++ src/com/company/FileInputClassExample.java
@@ -1,14 +1,15 @@
 package com.company;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 
 public class FileInputClassExample {
     public static void main(String[] args) {
         try {
             FileInputStream fi = new FileInputStream("/home/vasu/IdeaProjects/javaPrograms/src/com/company/inp.txt");
             FileOutputStream FO = new FileOutputStream("/home/vasu/IdeaProjects/javaPrograms/src/com/company/text.txt");
-            int i = 0, count=0;
+            int i, count=0;
             char[] save = new char[100];
             while ((i = fi.read())!=-1) {
                 System.out.print((char) i);
@@ -17,11 +18,9 @@ public class FileInputClassExample {
                 count++;
 
             }
-            byte[]b2=save.toString().getBytes();
+            byte[]b2= Arrays.toString(save).getBytes();
             FO.write(b2);
 
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        } catch (Exception e) {System.out.println(e);}
     }
 }
