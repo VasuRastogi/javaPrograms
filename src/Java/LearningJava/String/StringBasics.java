191db452fa2528312b6cc9b68976d64bf775d005
VasuRastogi <vasurastogi77@gmail.com>
2022-01-07 01:46:42 +0530

    StringBasics.java modified //contents added.

--- src/Java/LearningJava/String/StringBasics.java
+++ src/Java/LearningJava/String/StringBasics.java
@@ -12,25 +12,68 @@ public class StringBasics {
         String St3 = new String("Hello");          //using new keyword
         String St4 = new String("Hello");
 
-        System.out.println("----------");
+        System.out.println("-----charAt()-----");
         System.out.println(St3.charAt(0));   // charAt method
 
-        System.out.println("----------");
+        System.out.println("-----length()-----");
         System.out.println(St3.length());    //length method
-        System.out.println("----------");
 
+        System.out.println("-----String.format()-----");
         float arr = (float) 33.33;           // String.format method
         String.format("%53.2f ", arr );
         System.out.println(arr);
 
-        System.out.println("----------");
+        System.out.println("-----String.format2()-----");
         String str = String.format("%d", 1024);   // String.format method
         System.out.println(str);
 
-        //BeginIndex()
-        System.out.println("----------");
+        //substring()
+        System.out.println("-----substring()-----");
         String store = St3.substring(1); // substring(beginIndex, endIndex)
         System.out.println(store);
+
+        // contains()
+        System.out.println("-----contains()-----");
+        String Sttt = "Vasu Rastogi";
+        System.out.println(Sttt.contains("f"));  //output == true;
+
+        // join()
+        //join(CharSequence Delimiter, charSequence ....elements);
+        //public static String join(CharSequence delimiter, Iterable<? extends CharSequence> elements) //for arraylist.
+        System.out.println("-----join()-----");
+        String date = String.join("/", "26", "11", "2008");
+        String data = String.join("*","A", "B", "C", "D");
+        System.out.println(date);
+        System.out.println(data);
+
+        //equals()
+        System.out.println("-----equals()-----");
+        String s1="vasu";
+        String s2="vasu";
+        String s3="VASU";
+        System.out.println(s1.equals(s2));//true because content and case is same
+        System.out.println(s1.equals(s3));//false because case is not same
+
+        //isEmpty()
+        System.out.println("-----isEmpty()-----");
+        String s="";           // even space is considered as non-empty.
+        System.out.println(s.isEmpty());
+
+        //concat()
+        System.out.println("-----concat()-----");
+        String S1 = "Java String is";
+        S1.concat("not added");    // not added as not assigned.   /* IMP */
+        System.out.println(S1);    // will print previous & above will not be concatenated.
+        S1=S1.concat(" immutable(can't be changed back) so a clear assignment is required.");  /* IMP */
+        System.out.println(S1);    // this will print concatenated string.
+
+        //replace()
+        //replace(char/charSequence old/target , char/charSequence new/replacement)
+        System.out.println("-----replace()-----");
+        String S2="Hello i am vasu Rastogi";
+        S2=S2.replace("vasu" ,"Vasu");
+        S2.replace("i" ,"I");  //not work    //explicit (i.e. clear) assignment is required here.
+        System.out.println(S2);
 
 
     }
---
+++ src/Java/LearningJava/String/practice.java
@@ -0,0 +1,10 @@
+package Java.LearningJava.String;
+
+import java.lang.String;
+
+public class practice {
+    public static void main(String[] args) {
+        String str = "Vasu Rastogi";
+        System.out.print(str.contains("gi"));
+    }
+}
