cc61fb1eec3320f959a6754321e1b674e035ff3a
VasuRastogi <vasurastogi77@gmail.com>
2022-07-28 13:53:14 +0530

    added content to timeComplexity.java.

---
+++ src/Complexity/timeComplexity.java
@@ -0,0 +1,11 @@
+package Complexity;
+
+// Time complexity != Time taken in execution.
+
+// Time complexity is a function that tells us how the
+// time is going to grow as input grows.
+// 38 mins " https://www.youtube.com/watch?v=mV3wrLBbuuE&list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&index=24 "
+
+public class timeComplexity {
+
+}
--- src/Recursion/fibonacci.java
+++ src/Recursion/fibonacci.java
@@ -1,7 +1,9 @@
 package Recursion;
 
 // using an exponential time complexity algorithm to find Fibonacci numbers.
-
+// won't work on large number e.g. 50.
+// so, we use a different algo than this.
+// complexity = (1.6)^n
 
 public class fibonacci {
     public static void main(String[] args) {
