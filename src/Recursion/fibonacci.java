c973b28eb8e8694bb61abdad7c15b9f8a2a0c1f7
VasuRastogi <vasurastogi77@gmail.com>
2022-07-28 13:15:20 +0530

    fibonacci edited.

--- src/Recursion/fibonacci.java
+++ src/Recursion/fibonacci.java
@@ -1,4 +1,14 @@
 package Recursion;
 
 public class fibonacci {
+    public static void main(String[] args) {
+        int ans = fibo(10);
+        System.out.println(ans);
+    }
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
 }
